19ce3115c25cacf5d2f483fff315326686c7705d
smakarov2007 <sergey.makarov@list.ru>
2017-03-29 13:14:06 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+def application(environ, start_response):
+  status = '200 OK'
+  headers = [
+    'Content-Type', 'text/plain'
+  ]
+  body = "\r\n".join(environ['QUERY_STRING'].split("&"))
+  start_response(status, headers)
+  return body
