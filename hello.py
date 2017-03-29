ed85de297776639432f6eb88cf403a19978a385d
smakarov2007 <sergey.makarov@list.ru>
2017-03-29 16:58:44 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
   status = '200 OK'
   headers = [('Content-Type', 'text/plain')]
-  body = environ['QUERY_STRING'].split("&")
+  qs = parse_qs(environ['QUERY_STRING'])
   start_response(status, headers)
-  return ['%s=%<br>' % (k, body[k][0] for k in body)]
+  return ['%s=%<br>' % (k, qs[k][0] for k in qs)]
