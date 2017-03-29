7e9b0643f777a70002712f72c020bc62e42b4c83
smakarov2007 <sergey.makarov@list.ru>
2017-03-29 15:34:01 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 def application(environ, start_response):
   status = '200 OK'
-  headers = [
-    'Content-Type', 'text/plain'
-  ]
-  body = "\r\n".join(environ['QUERY_STRING'].split("&"))
+  headers = [('Content-Type', 'text/plain')]
+  body = "\n".join(environ['QUERY_STRING'].split("&"))
   start_response(status, headers)
   return body
