c488e6b7aa6a5b3deab603745419a72bbb91ae0f
smakarov2007 <sergey.makarov@list.ru>
2017-03-30 11:21:49 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+cd /home/box/web
+sudo gunicorn -b 0.0.0.0:8080 hello:app
 sudo /etc/init.d/mysql start
