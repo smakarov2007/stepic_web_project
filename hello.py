5f0e5e619b6a6e89fa1f628c2d1902a71cf001de
smakarov2007 <sergey.makarov@list.ru>
2017-03-30 11:19:51 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(environ, start_response):
   start_response('200 OK', [('Content-Type', 'text/plain')])
   qs = environ['QUERY_STRING']
-  return [bytes(i+'\n') for i in qs.split('&')]
+  return [bytes(i+'\n','utf-8') for i in qs.split('&')]
